3487b8c111dd27b03c6d1db88a6069b11c9ffa00
ing <inyoman_gurnitha@yahoo.com>
2020-01-15 12:11:38 +0700

    43. Configure header footer file in dashboard-php

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Inventory POS System version 1.0
+    </div>
+    <!-- Default to the left -->
+    <strong>Developed by & &copy; 2020 <a href="#">Tika&Ing</a>.</strong> All rights reserved.
+  </footer>
---
+++ inc/scripts.php
@@ -0,0 +1,12 @@
+<!-- jQuery 3 -->
+<script src="assets/jquery/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets/AdminLTE/js/adminlte.min.js"></script>
+
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+     Both of these plugins are recommended to enhance the
+     user experience. -->
+</body>
+</html>
